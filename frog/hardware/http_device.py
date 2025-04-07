454c70eab95f770e3514b81ebe28bc760f5e10f1
Alex Dewar <a.dewar@imperial.ac.uk>
2025-04-07 16:53:37 +0100

    NO TESTS - HTTPDevice: Allow setting number of retries

--- frog/hardware/http_device.py
+++ frog/hardware/http_device.py
@@ -13,32 +13,45 @@ from frog.hardware.device import AbstractDevice
 class HTTPDevice(
     AbstractDevice,
     parameters={
-        "timeout": "The maximum time in seconds to wait for a response from the server"
+        "timeout": "The maximum time in seconds to wait for a response from the server",
+        "default_retries": "The default number of times to retry a request if it fails",
     },
 ):
     """A device which communicates via HTTP requests."""
 
-    def __init__(self, timeout: float) -> None:
+    def __init__(self, timeout: float, default_retries: int) -> None:
         """Create a new HTTPDevice.
 
         Args:
             timeout: The maximum time in seconds to wait for a response from the server
+            default_retries: The default number of times to retry a failed request
         """
         if timeout <= 0:
             raise ValueError("Timeout must be greater than zero")
+        if default_retries < 0:
+            raise ValueError("Retries must be greater than or equal to zero")
 
         self._timeout = timeout
+        self._retries = default_retries
         self._manager = QNetworkAccessManager()
 
     def make_request(
-        self, url: str, callback: Callable[[str], Any] | None = None
+        self,
+        url: str,
+        callback: Callable[[str], Any] | None = None,
+        retries: int | None = None,
     ) -> None:
         """Make a new HTTP request in the background.
 
         Args:
             url: The URL to connect to
             callback: A function to be called when a successful response is received
+            retries: Number of times to retry the request
         """
+        if retries is None:
+            retries = self._retries
+        elif retries < 0:
+            raise ValueError("Retries must be greater than or equal to zero")
         if not callback:
             callback = self.handle_response
 
@@ -46,7 +59,13 @@ class HTTPDevice(
         request.setTransferTimeout(round(1000 * self._timeout))
         reply = self._manager.get(request)
         reply.finished.connect(
-            partial(self.pubsub_errors(self._on_reply_received), reply, callback)
+            partial(
+                self.pubsub_errors(self._on_reply_received),
+                reply,
+                url,
+                callback,
+                retries,
+            )
         )
 
     @abstractmethod
@@ -54,11 +73,20 @@ class HTTPDevice(
         """The default callback for successful HTTP responses."""
 
     def _on_reply_received(
-        self, reply: QNetworkReply, callback: Callable[[str], Any]
+        self,
+        reply: QNetworkReply,
+        url: str,
+        callback: Callable[[str], Any],
+        retries_remaining: int,
     ) -> None:
         """Handle received HTTP reply."""
         if reply.error() != QNetworkReply.NetworkError.NoError:
-            raise RuntimeError(f"Network error: {reply.errorString()}")
+            if retries_remaining == 0:
+                raise RuntimeError(f"Network error: {reply.errorString()}")
+
+            # ...otherwise, try again
+            self.make_request(url, callback, retries_remaining - 1)
+            return
 
         # Parse the received message
         data: bytes = reply.readAll().data()
