44217f6417354f3d464479121c817e333062a4d8
Alex Dewar <a.dewar@imperial.ac.uk>
2025-09-04 17:30:28 +0100

    Fix paths in API docs generation script

--- docs/gen_ref_nav.py
+++ docs/gen_ref_nav.py
@@ -6,12 +6,12 @@ import mkdocs_gen_files
 
 nav = mkdocs_gen_files.Nav()
 
-for path in sorted(Path("frog").glob("**/*.py")):
+for path in sorted(Path("src/frog").glob("**/*.py")):
     module_path = path.relative_to(".").with_suffix("")
-    doc_path = path.relative_to(".").with_suffix(".md")
+    doc_path = Path(*path.parts[1:]).relative_to(".").with_suffix(".md")
     full_doc_path = Path("reference", doc_path)
 
-    parts = list(module_path.parts)
+    parts = list(module_path.parts[1:])
     if ".array_cache" in parts:
         continue
     elif parts[-1] == "__init__":
@@ -26,7 +26,7 @@ for path in sorted(Path("frog").glob("**/*.py")):
         ident = ".".join(parts)
         print("::: " + ident, file=fd)
 
-    mkdocs_gen_files.set_edit_path(full_doc_path, Path("../") / path)
+    mkdocs_gen_files.set_edit_path(full_doc_path, Path("..", *path.parts[1:]))
 
 with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
     nav_file.writelines(nav.build_literate_nav())
