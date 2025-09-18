ImperialCollegeLondon/FINESSE_GUI

Move import hardware set functionality to menu

finesse/gui/hardware_set/menu.py
@@ -1,6 +1,13 @@
 """Provides a menu for working with hardware sets."""
 
-from PySide6.QtWidgets import QMenu
+from pathlib import Path
+
+from pubsub import pub
+from PySide6.QtGui import QAction
+from PySide6.QtWidgets import QFileDialog, QMenu
+
+from finesse.gui.error_message import show_error_message
+from finesse.gui.hardware_set.hardware_set import HardwareSet
 
 
 class HardwareSetsMenu(QMenu):
@@ -9,3 +16,26 @@ class HardwareSetsMenu(QMenu):
     def __init__(self) -> None:
         """Create a new HardwareSetsMenu."""
         super().__init__("Hardware sets")
+
+        import_action = QAction("Import from file", self)
+        import_action.triggered.connect(self._import_hardware_set)
+        self.addAction(import_action)
+
+    def _import_hardware_set(self) -> None:
+        """Import a hardware set from a file."""
+        file_path, _ = QFileDialog.getOpenFileName(
+            self, "Import hardware set config file", filter="*.yaml"
+        )
+        if not file_path:
+            return
+
+        try:
+            hw_set = HardwareSet.load(Path(file_path))
+        except Exception:
+            show_error_message(
+                self,
+                "Could not load hardware set config file. Is it in the correct format?",
+                "Could not load config file",
+            )
+        else:
+            pub.sendMessage("hardware_set.add", hw_set=hw_set)

tests/gui/hardware_set/conftest.py
@@ -6,7 +6,9 @@ from pathlib import Path
 import pytest
 
 from finesse.gui.hardware_set.device import OpenDeviceArgs
-from finesse.gui.hardware_set.hardware_set import HardwareSet
+from finesse.gui.hardware_set.hardware_set import (
+    HardwareSet,
+)
 
 _HW_SETS = (
     HardwareSet(

tests/gui/hardware_set/test_menu.py
@@ -1,0 +1,79 @@
+"""Tests for the HardwareSetsMenu class."""
+
+from pathlib import Path
+from unittest.mock import MagicMock, Mock, patch
+
+import pytest
+
+from finesse.gui.hardware_set.hardware_set import (
+    HardwareSet,
+)
+from finesse.gui.hardware_set.menu import HardwareSetsMenu
+
+
+@pytest.fixture()
+def hw_menu(qtbot) -> HardwareSetsMenu:
+    """A fixture providing a HardwareSetsMenu."""
+    return HardwareSetsMenu()
+
+
+@patch.object(HardwareSet, "load")
+@patch("finesse.gui.hardware_set.menu.show_error_message")
+@patch("finesse.gui.hardware_set.menu.QFileDialog.getOpenFileName")
+def test_import_hardware_set_success(
+    open_file_mock: Mock,
+    error_message_mock: Mock,
+    load_mock: Mock,
+    hw_menu: HardwareSetsMenu,
+    sendmsg_mock: MagicMock,
+    qtbot,
+) -> None:
+    """Test the _import_hardware_set() method when a file is loaded successfully."""
+    path = Path("dir/file.txt")
+    hw_set = MagicMock()
+    load_mock.return_value = hw_set
+    open_file_mock.return_value = (str(path), None)
+    hw_menu._import_hardware_set()
+    load_mock.assert_called_once_with(path)
+    sendmsg_mock.assert_called_once_with("hardware_set.add", hw_set=hw_set)
+    error_message_mock.assert_not_called()
+
+
+@patch.object(HardwareSet, "load")
+@patch("finesse.gui.hardware_set.menu.show_error_message")
+@patch("finesse.gui.hardware_set.menu.QFileDialog.getOpenFileName")
+def test_import_hardware_set_cancelled(
+    open_file_mock: Mock,
+    error_message_mock: Mock,
+    load_mock: Mock,
+    hw_menu: HardwareSetsMenu,
+    sendmsg_mock: MagicMock,
+    qtbot,
+) -> None:
+    """Test the _import_hardware_set() method when the dialog is closed."""
+    open_file_mock.return_value = (None, None)
+    hw_menu._import_hardware_set()
+    sendmsg_mock.assert_not_called()
+    error_message_mock.assert_not_called()
+    load_mock.assert_not_called()
+
+
+@patch.object(HardwareSet, "load")
+@patch("finesse.gui.hardware_set.menu.show_error_message")
+@patch("finesse.gui.hardware_set.menu.QFileDialog.getOpenFileName")
+def test_import_hardware_set_error(
+    open_file_mock: Mock,
+    error_message_mock: Mock,
+    load_mock: Mock,
+    hw_menu: HardwareSetsMenu,
+    sendmsg_mock: MagicMock,
+    qtbot,
+) -> None:
+    """Test the _import_hardware_set() method when a file fails to load."""
+    path = Path("dir/file.txt")
+    load_mock.side_effect = RuntimeError
+    open_file_mock.return_value = (str(path), None)
+    hw_menu._import_hardware_set()
+    load_mock.assert_called_once_with(path)
+    sendmsg_mock.assert_not_called()
+    error_message_mock.assert_called_once()

ImperialCollegeLondon/FROG

src/frog/gui/hardware_set/menu.py
@@ -1,6 +1,13 @@
 """Provides a menu for working with hardware sets."""
 
-from PySide6.QtWidgets import QMenu
+from pathlib import Path
+
+from pubsub import pub
+from PySide6.QtGui import QAction
+from PySide6.QtWidgets import QFileDialog, QMenu
+
+from frog.gui.error_message import show_error_message
+from frog.gui.hardware_set.hardware_set import HardwareSet
 
 
 class HardwareSetsMenu(QMenu):
@@ -9,3 +16,26 @@ class HardwareSetsMenu(QMenu):
     def __init__(self) -> None:
         """Create a new HardwareSetsMenu."""
         super().__init__("Hardware sets")
+
+        import_action = QAction("Import from file", self)
+        import_action.triggered.connect(self._import_hardware_set)
+        self.addAction(import_action)
+
+    def _import_hardware_set(self) -> None:
+        """Import a hardware set from a file."""
+        file_path, _ = QFileDialog.getOpenFileName(
+            self, "Import hardware set config file", filter="*.yaml"
+        )
+        if not file_path:
+            return
+
+        try:
+            hw_set = HardwareSet.load(Path(file_path))
+        except Exception:
+            show_error_message(
+                self,
+                "Could not load hardware set config file. Is it in the correct format?",
+                "Could not load config file",
+            )
+        else:
+            pub.sendMessage("hardware_set.add", hw_set=hw_set)

tests/gui/hardware_set/conftest.py
@@ -6,7 +6,9 @@ from pathlib import Path
 import pytest
 
 from frog.gui.hardware_set.device import OpenDeviceArgs
-from frog.gui.hardware_set.hardware_set import HardwareSet
+from frog.gui.hardware_set.hardware_set import (
+    HardwareSet,
+)
 
 _HW_SETS = (
     HardwareSet(

tests/gui/hardware_set/test_menu.py
@@ -1,0 +1,79 @@
+"""Tests for the HardwareSetsMenu class."""
+
+from pathlib import Path
+from unittest.mock import MagicMock, Mock, patch
+
+import pytest
+
+from frog.gui.hardware_set.hardware_set import (
+    HardwareSet,
+)
+from frog.gui.hardware_set.menu import HardwareSetsMenu
+
+
+@pytest.fixture()
+def hw_menu(qtbot) -> HardwareSetsMenu:
+    """A fixture providing a HardwareSetsMenu."""
+    return HardwareSetsMenu()
+
+
+@patch.object(HardwareSet, "load")
+@patch("frog.gui.hardware_set.menu.show_error_message")
+@patch("frog.gui.hardware_set.menu.QFileDialog.getOpenFileName")
+def test_import_hardware_set_success(
+    open_file_mock: Mock,
+    error_message_mock: Mock,
+    load_mock: Mock,
+    hw_menu: HardwareSetsMenu,
+    sendmsg_mock: MagicMock,
+    qtbot,
+) -> None:
+    """Test the _import_hardware_set() method when a file is loaded successfully."""
+    path = Path("dir/file.txt")
+    hw_set = MagicMock()
+    load_mock.return_value = hw_set
+    open_file_mock.return_value = (str(path), None)
+    hw_menu._import_hardware_set()
+    load_mock.assert_called_once_with(path)
+    sendmsg_mock.assert_called_once_with("hardware_set.add", hw_set=hw_set)
+    error_message_mock.assert_not_called()
+
+
+@patch.object(HardwareSet, "load")
+@patch("frog.gui.hardware_set.menu.show_error_message")
+@patch("frog.gui.hardware_set.menu.QFileDialog.getOpenFileName")
+def test_import_hardware_set_cancelled(
+    open_file_mock: Mock,
+    error_message_mock: Mock,
+    load_mock: Mock,
+    hw_menu: HardwareSetsMenu,
+    sendmsg_mock: MagicMock,
+    qtbot,
+) -> None:
+    """Test the _import_hardware_set() method when the dialog is closed."""
+    open_file_mock.return_value = (None, None)
+    hw_menu._import_hardware_set()
+    sendmsg_mock.assert_not_called()
+    error_message_mock.assert_not_called()
+    load_mock.assert_not_called()
+
+
+@patch.object(HardwareSet, "load")
+@patch("frog.gui.hardware_set.menu.show_error_message")
+@patch("frog.gui.hardware_set.menu.QFileDialog.getOpenFileName")
+def test_import_hardware_set_error(
+    open_file_mock: Mock,
+    error_message_mock: Mock,
+    load_mock: Mock,
+    hw_menu: HardwareSetsMenu,
+    sendmsg_mock: MagicMock,
+    qtbot,
+) -> None:
+    """Test the _import_hardware_set() method when a file fails to load."""
+    path = Path("dir/file.txt")
+    load_mock.side_effect = RuntimeError
+    open_file_mock.return_value = (str(path), None)
+    hw_menu._import_hardware_set()
+    load_mock.assert_called_once_with(path)
+    sendmsg_mock.assert_not_called()
+    error_message_mock.assert_called_once()